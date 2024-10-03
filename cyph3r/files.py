8b5585921260d46469826c0f7a372bd18626db4b
adeola <adeola.akinsanya@gmail.com>
2024-10-03 14:44:31 -0400

    refactor files module

--- cyph3r/files.py
+++ cyph3r/files.py
@@ -1,29 +1,55 @@
+import os
 from .crypto import CryptoManager
 from django.conf import settings
 from django import forms
-import os
 
-"""
-Helper functions to create files containing encrypted secret keys and key shares.
+# Constants for file extensions
+GPG_FILE_EXTENSION = ".txt.gpg"
 
-"""
-###########################################
+
+##########################################
+# Helper Functions for File Manipulation #
+##########################################
+
+
+def create_directory_if_not_exists(directory_path: str):
+    """Creates a directory if it doesn't already exist."""
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+
+def save_encrypted_data_to_file(file_path: str, encrypted_data: bytes):
+    """Saves encrypted data to a file."""
+    with open(file_path, "wb") as fp:
+        fp.write(encrypted_data)
+
+
+def import_pgp_key_from_file(cm: CryptoManager, file):
+    """Imports PGP key from a file into the CryptoManager."""
+    file.seek(0)
+    cm.gpg.import_keys(file.read())
+    imported_key = cm.gpg.list_keys()[-1]
+    return imported_key["fingerprint"], imported_key["keyid"]
+
+
+##########################################
 # Wireless Module File Creation Functions #
-###########################################
+##########################################
 
 
 def create_wireless_wrapped_secret_key_file(
     cm: CryptoManager, wrapped_data: bytes, protocol: str, key_type: str
 ) -> str:
-    # Write Encrypted key to file for download.
+    """Writes Encrypted key to a file for download."""
     wrapped_data_file_name = f"{protocol}-{key_type}-wrapped-secret.txt"
-
     save_path = os.path.join(settings.MEDIA_ROOT, wrapped_data_file_name)
+
     with open(save_path, "wb") as fp:
         data = cm.wrapped_key_text_format(
             protocol, key_type, cm.bytes_to_hex(wrapped_data)
         )
         fp.write(data)
+
     return wrapped_data_file_name
 
 
@@ -37,152 +63,85 @@ def create_wireless_provider_encrypted_key_files(
     number_of_shares: int,
 ) -> list:
     """Creates XOR key shares encrypted with Provider PGP keys and saves them to files."""
-
     # Split the secret key into shares using XOR splitting
     shares = cm.xor_split_secret(secret_key, key_size, number_of_shares)
 
-    # Initialize the list to store the filenames of the encrypted keys (or shares) for download
+    # Initialize the list to store the paths to the encrypted keys (or shares) for download
     provider_files = []
 
-    # Loop through each uploaded provider's public key file
+    # Loop through each uploaded provider's public key file and encrypt the XOR key share with it
     for key_index, file in enumerate(
         form.cleaned_data["provider_public_keys"], start=1
     ):
-        # Reset the file pointer to the beginning of the file
-        file.seek(0)
-
-        # Import the Provider's PGP public keys from the uploaded file
-        cm.gpg.import_keys(file.read())
-
-        # Retrieve the latest imported key's fingerprint and keyid
-        imported_key = cm.gpg.list_keys()[-1]
-        fingerprint = imported_key["fingerprint"]
-        keyid = imported_key["keyid"]
-
-        # Generate Key Check Value (KCV) for the current key share
+        fingerprint, keyid = import_pgp_key_from_file(cm, file)
         kcv = cm.generate_kcv(shares[key_index - 1])
-
-        # Convert the key share to a hex string format
         xor_key_share_hex = cm.bytes_to_hex(shares[key_index - 1])
-
-        # Create the filename for the encrypted key component and append filename to provider_files list for download
         provider_file_name = (
-            f"G+D-{protocol}-{key_type}-key-{key_index}-{keyid}.txt.gpg"
+            f"G+D-{protocol}-{key_type}-key-{key_index}-{keyid}{GPG_FILE_EXTENSION}"
         )
         save_path = os.path.join(settings.MEDIA_ROOT, provider_file_name)
         provider_files.append(provider_file_name)
 
-        # Prepare provider key share data in the requested text format
         gd_key_share_data = cm.gd_text_format(
             xor_key_share_hex, kcv, key_type, key_size, key_index
         )
-
-        # Encrypt the key share data using the provider's public key
         encrypted_data = cm.gpg.encrypt(
             gd_key_share_data, fingerprint, always_trust=True, armor=False
         )
+        save_encrypted_data_to_file(save_path, encrypted_data.data)
 
-        # Save the encrypted key share to the designated file
-        with open(save_path, "wb") as fp:
-            fp.write(encrypted_data.data)
-
-    # Return Provider files list for download
+    # Return the path to the encrypted provider files for download
     return provider_files
 
 
 def create_wireless_security_officers_encrypted_key_files(
-    form: forms.Form,
-    cm: CryptoManager,
-    key_type: str,
-    protocol: str,
-    shares: list,
+    form: forms.Form, cm: CryptoManager, key_type: str, protocol: str, shares: list
 ) -> list:
     """Encrypts files containing the Shamir wrap key share with security officer (SO) PGP keys."""
-    # Initialize the list to store the file names of the encrypted shares for download
+    # Initialize the list to store the paths to the encrypted shares for download
     security_officer_file_names = []
 
-    # Loop through each uploaded SO's public key file
+    # Loop through each uploaded SO's public key files and encrypt the Shamir wrap key shares with them
     for key_index, file in enumerate(
         form.cleaned_data["security_officers_public_keys"], start=1
     ):
-        # Reset the file pointer to the beginning of the file
-        file.seek(0)
-
-        # Import the SO's PGP public keys from the uploaded file
-        cm.gpg.import_keys(file.read())
-
-        # Retrieve the latest imported key's fingerprint and keyid
-        imported_key = cm.gpg.list_keys()[-1]
-        fingerprint = imported_key["fingerprint"]
-        keyid = imported_key["keyid"]
-
-        # Create the filename for the encrypted key component and append filename to security_officer_files list for download
+        fingerprint, keyid = import_pgp_key_from_file(cm, file)
         security_officer_file_name = (
-            f"SO-{protocol}-{key_type}-wrap-key-{key_index}-{keyid}.txt.gpg"
+            f"SO-{protocol}-{key_type}-wrap-key-{key_index}-{keyid}{GPG_FILE_EXTENSION}"
         )
         save_path = os.path.join(settings.MEDIA_ROOT, security_officer_file_name)
         security_officer_file_names.append(security_officer_file_name)
 
-        # Extract wrap key share from the list of shares and convert share from bytes to human readable hex string
-        # Allows Security officer to easily reconstruct the wrap key but care should be taken as it is human readable
         wrap_key_share_hex = cm.bytes_to_hex(shares[key_index - 1][1])
-
-        # Prepare security officer file content
         so_wrap_key_share_data = cm.so_text_format(
             wrap_key_share_hex, protocol, key_type, key_index
         )
-
-        # Encrypt the key share data using the provider's public key
         encrypted_so_wrap_key_share_data = cm.gpg.encrypt(
             so_wrap_key_share_data, fingerprint, always_trust=True, armor=False
         )
+        save_encrypted_data_to_file(save_path, encrypted_so_wrap_key_share_data.data)
 
-        # Save the encrypted key share to the designated file
-        with open(save_path, "wb") as fp:
-            fp.write(encrypted_so_wrap_key_share_data.data)
-
-    # Return Provider files list for download
+    # Return the path to the SO encrypted files for download
     return security_officer_file_names
 
 
 def create_wireless_milenage_encrypted_file(
-    form: forms.Form,
-    cm: CryptoManager,
-    secret_key: bytes,
-    key_type: str,
-):
-    """Wraps the secret key with PGP public key of engineer entering milenage keys in provider terminal and saves it to gile."""
-
+    form: forms.Form, cm: CryptoManager, secret_key: bytes, key_type: str
+) -> str:
+    """Wraps the secret key with PGP public key of engineer entering milenage keys in provider terminal and saves it to file."""
     file = form.cleaned_data["milenage_public_key"]
+    fingerprint, keyid = import_pgp_key_from_file(cm, file)
 
-    # Reset the file pointer to the beginning of the file
-    file.seek(0)
-
-    # Import the engineer's PGP public keys from the uploaded file
-    cm.gpg.import_keys(file.read())
-
-    # Retrieve the latest imported key's fingerprint and keyid
-    imported_key = cm.gpg.list_keys()[-1]
-    fingerprint = imported_key["fingerprint"]
-    keyid = imported_key["keyid"]
-
-    # Convert the secret key to a hex string format
     secret_key_hex = cm.bytes_to_hex(secret_key)
-
-    # Create the filename for the encrypted key and append filename to security_officer_files list for download
-    milenage_file_name = f"milenage-{key_type}-key-{keyid}.txt.gpg"
+    milenage_file_name = f"milenage-{key_type}-key-{keyid}{GPG_FILE_EXTENSION}"
     save_path = os.path.join(settings.MEDIA_ROOT, milenage_file_name)
 
-    # Encrypt the key share data using the provider's public key
     encrypted_data = cm.gpg.encrypt(
         secret_key_hex, fingerprint, always_trust=True, armor=False
     )
+    save_encrypted_data_to_file(save_path, encrypted_data.data)
 
-    # Save the encrypted secret key to the designated file
-    with open(save_path, "wb") as fp:
-        fp.write(encrypted_data.data)
-
-    # Return the filename of the encrypted milenage keys for download
+    # Return the path to the encrypted milenage key files for download
     return milenage_file_name
 
 
@@ -195,129 +154,86 @@ def write_key_share_so_public_keys_to_disk(
     form: forms.Form, user_directory: str
 ) -> list:
     """Writes the Security Officer's public keys to a file to encrypt key or key-share."""
+    path = os.path.join(settings.MEDIA_ROOT, user_directory)
+    create_directory_if_not_exists(path)
 
-    # Create the directory if it doesn't exist
-    path = settings.MEDIA_ROOT / user_directory
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    # Initialize the list to store the file names of the public keys
+    # Initialize the list to store the paths to the public key files
     key_share_public_key_files = []
 
-    # Write uploaded public keys to disk
+    # Loop through the uploaded public key files and write them to disk
     for file in form.cleaned_data["key_share_public_keys"]:
-        # Reset the file pointer to the beginning of the file
-        file.seek(0)
         save_path = os.path.join(path, file.name)
-
         with open(save_path, "wb") as fp:
             for chunk in file.chunks():
                 fp.write(chunk)
 
-        # Append file name to list of public key files
         key_share_public_key_files.append(save_path)
 
-    # Return the list of file paths to the public key files
+    # Return the path to the public key files for key sharing operations
     return key_share_public_key_files
 
 
 def create_key_share_reconstruct_secret_file(
-    cm: CryptoManager,
-    secret_key: bytes,
-    user_directory: str,
-    public_key_files: list,
-):
+    cm: CryptoManager, secret_key: bytes, user_directory: str, public_key_files: list
+) -> list:
     """Wraps the reconstructed secret key with PGP public key."""
-    # Create the directory if it doesn't exist
-    path = settings.MEDIA_ROOT / user_directory
-    if not os.path.exists(path):
-        os.makedirs(path)
+    path = os.path.join(settings.MEDIA_ROOT, user_directory)
+    create_directory_if_not_exists(path)
 
-    # Initialize the list to store the file names of the encrypted secret
+    # Initialize the list to store the file names of the encrypted secret key
     key_share_files_names = []
 
+    # Loop through the uploaded public key file and encrypt the reconstructed secret key
     for file in public_key_files:
         with open(file, "rb") as fp:
-
-            # Import the PGP public key from the uploaded file
             cm.gpg.import_keys(fp.read())
 
-        # Retrieve the latest imported key's fingerprint and keyid
         imported_key = cm.gpg.list_keys()[-1]
-        fingerprint = imported_key["fingerprint"]
-        keyid = imported_key["keyid"]
-
-        # Create the filename for the encrypted key and append filename to keys_share_files list for download
-        file_name = f"key-share-reconstructed-secret-{keyid}.txt.gpg"
+        fingerprint, keyid = imported_key["fingerprint"], imported_key["keyid"]
+        file_name = f"key-share-reconstructed-secret-{keyid}{GPG_FILE_EXTENSION}"
         save_path = os.path.join(path, file_name)
         key_share_files_names.append(file_name)
 
-        # Encrypt the secret with PGP public key
         encrypted_data = cm.gpg.encrypt(
             cm.bytes_to_hex(secret_key), fingerprint, always_trust=True, armor=False
         )
+        save_encrypted_data_to_file(save_path, encrypted_data.data)
 
-        # Save the encrypted secret key to the designated file save path
-        with open(save_path, "wb") as fp:
-            fp.write(encrypted_data.data)
-
-    # Return the filename of the encrypted milenage keys for download
+    # Return the file name of the encrypted secret key file for download
     return key_share_files_names
 
 
 def create_key_share_split_secret_files(
-    cm: CryptoManager,
-    shares: list,
-    user_directory: str,
-    public_key_files: list,
-):
-    # Create the directory if it doesn't exist
-    path = settings.MEDIA_ROOT / user_directory
-    if not os.path.exists(path):
-        os.makedirs(path)
+    cm: CryptoManager, shares: list, user_directory: str, public_key_files: list
+) -> list:
+    """Splits the secret key into shares and encrypts each share."""
+    path = os.path.join(settings.MEDIA_ROOT, user_directory)
+    create_directory_if_not_exists(path)
 
-    # Initialize the list to store the file names of the encrypted secret
+    # Initialize the list to store the file names of the encrypted shares
     key_share_files_names = []
 
+    # Loop through the uploaded public key files and encrypt the secret key shares
     for idx, file in enumerate(public_key_files, start=1):
         with open(file, "rb") as fp:
-            # Reset the file pointer to the beginning of the file
             fp.seek(0)
-
-            # Import the PGP public key from the uploaded file
             cm.gpg.import_keys(fp.read())
 
-        # Retrieve the latest imported key's fingerprint and keyid
         imported_key = cm.gpg.list_keys()[-1]
-        fingerprint = imported_key["fingerprint"]
-        keyid = imported_key["keyid"]
-
-        # Create the filename for the encrypted key and append filename to keys_share_files list for download
-        file_name = f"key-share-split-secret-index-{idx}-{keyid}.txt.gpg"
+        fingerprint, keyid = imported_key["fingerprint"], imported_key["keyid"]
+        file_name = f"key-share-split-secret-index-{idx}-{keyid}{GPG_FILE_EXTENSION}"
         save_path = os.path.join(path, file_name)
-
-        # Append the file name to the list of key share files
         key_share_files_names.append(file_name)
 
-        # Encrypt the secret with PGP public key
+        share_data = (
+            shares[idx - 1][1]
+            if isinstance(shares[idx - 1], tuple)
+            else shares[idx - 1]
+        )
+        encrypted_data = cm.gpg.encrypt(
+            cm.bytes_to_hex(share_data), fingerprint, always_trust=True, armor=False
+        )
+        save_encrypted_data_to_file(save_path, encrypted_data.data)
 
-        if isinstance(shares[idx - 1], tuple):
-            encrypted_data = cm.gpg.encrypt(
-                cm.bytes_to_hex(shares[idx - 1][1]),
-                fingerprint,
-                always_trust=True,
-                armor=False,
-            )
-        else:
-            encrypted_data = cm.gpg.encrypt(
-                cm.bytes_to_hex(shares[idx - 1]),
-                fingerprint,
-                always_trust=True,
-                armor=False,
-            )
-        # Save the encrypted secret key to the designated file save path
-        with open(save_path, "wb") as fp:
-            fp.write(encrypted_data.data)
-
-    # Return the filename of the encrypted milenage keys for download
+    # Return the file namea of the encrypted secret key shares for download
     return key_share_files_names
