7de1b856c53604c830759141f5e5147cfbbf1ecd
adeola <adeola.akinsanya@gmail.com>
2024-10-04 23:13:37 -0400

    update requirements.txt and sesstings.py django-db-url

--- cyphr/settings.py
+++ cyphr/settings.py
@@ -86,12 +86,9 @@ class Dev(Configuration):
     # Database
     # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+    DATABASES = values.DatabaseURLValue(
+        f"sqlite:///{BASE_DIR}/db.sqlite3", environ_prefix="DJANGO"
+    )
 
     # Password validation
     # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
