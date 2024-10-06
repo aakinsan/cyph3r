23aadbd18176d63ec6235693e9a0560e829eb581
adeola <adeola.akinsanya@gmail.com>
2024-10-06 14:25:08 -0400

    move dev secret_key to env file

--- cyphr/settings.py
+++ cyphr/settings.py
@@ -23,7 +23,7 @@ class Dev(Configuration):
     # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = "django-insecure-xv4lnz$#1#k)lj3a+mz9i6y1n&swfexqfboa&q(%pbj53u#9bv"
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
