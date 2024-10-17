8907360be58b26c93b7632ac8416477f1e433d88
adeola <adeola.akinsanya@gmail.com>
2024-10-17 05:28:59 -0400

    update delete_old_files.py

--- cyph3r/management/commands/delete_old_files.py
+++ cyph3r/management/commands/delete_old_files.py
@@ -3,6 +3,10 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 import os
 import shutil
+import logging
+
+# Define the logger for the module
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -10,21 +14,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         now = time.time()
+        if settings.DEBUG:
+            media_root = settings.MEDIA_ROOT
+        else:
+            media_root = os.getenv("MEDIA_ROOT")
 
-        time_limit = 3 * 60 * 60  # 4 hours in seconds
+        time_limit = 1 * 60 * 60  # 4 hours in seconds
 
-        for f in os.listdir(settings.MEDIA_ROOT):
-            path = os.path.join(settings.MEDIA_ROOT, f)
+        for f in os.listdir(media_root):
+            path = os.path.join(media_root, f)
 
             # Check last modification time of file or folder has not exceeded time limit
             if now - os.path.getmtime(path) > time_limit:
                 try:
                     if os.path.isdir(path):
                         shutil.rmtree(path)
+                        logger.info(f"Successfully deleted {path}")
                     else:
                         os.remove(path)
-                    self.stdout.write(
-                        self.style.SUCCESS(f"Successfully deleted {path}")
-                    )
+                    logger.info(f"Successfully deleted {path}")
                 except Exception as e:
-                    self.stderr.write(self.style.ERROR(f"Error deleting {path}: {e}"))
+                    logger.error(
+                        f"An error occurred in the index view: {e}", exc_info=True
+                    )
