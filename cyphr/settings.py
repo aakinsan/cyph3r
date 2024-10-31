231220c50ea5f567f96bc075a2c2863f32387edd
adeola <adeola.akinsanya@cogeco.com>
2024-10-30 22:41:06 -0400

    update django dev secret

--- cyphr/settings.py
+++ cyphr/settings.py
@@ -26,7 +26,7 @@ class Dev(Configuration):
     # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = get_secret(id="django_secret")
+    SECRET_KEY = os.getenv("DJANGO_DEV_SECRET_KEY")
 
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
